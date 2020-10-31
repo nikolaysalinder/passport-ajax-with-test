dafbcbf3c344aeac573cf9011494526d91d41ea3
Nikolay Salinder <nikolay.salinder@gmail.com>
2020-10-31 17:39:39 +0500

    Anded test

---
+++ fixtures/index.js
@@ -0,0 +1,14 @@
+const User = require('../models/User');
+const mongoose = require('../libs/mongoose');
+const users = require('./users');
+
+(async () => {
+  await User.remove();
+  for (var i = 0; i < users.length;  i++) {
+    await User.create(users[i]);
+  }
+
+  mongoose.disconnect();
+
+  console.log(`All done, ${users.length} users have been saved in DB`);
+})()
